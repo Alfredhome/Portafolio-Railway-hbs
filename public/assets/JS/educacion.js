415b1468869c4ae649da8cc43a4b537df72a24cf
Alfredhome <fcojs@miumg.edu.gt>
2023-10-18 19:20:11 -0600

    primer commit

---
+++ public/assets/JS/educacion.js
@@ -0,0 +1,64 @@
+/* const slideshow = document.querySelector(".slideshow-contenedor");
+const slides = document.querySelectorAll(".slide");
+const anteriorBtn = document.querySelector(".anterior");
+const siguienteBtn = document.querySelector(".siguiente");
+
+let contador = 0;
+
+siguienteBtn.addEventListener("click", () => {
+    contador++;
+    if (contador >= slides.length) {
+        contador = 0;
+    }
+    actualizarSlide();
+});
+
+anteriorBtn.addEventListener("click", () => {
+    contador--;
+    if (contador < 0) {
+        contador = slides.length -1;
+    }
+    actualizarSlide();
+});
+
+function actualizarSlide() {
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${(index - contador) * 100}%)`;
+    });
+}
+
+actualizarSlide();
+ */
+const slides = document.querySelectorAll(".slide");
+const anteriorBtn = document.querySelector(".anterior");
+const siguienteBtn = document.querySelector(".siguiente");
+
+let contador = 0;
+
+siguienteBtn.addEventListener("click", () => {
+    contador++;
+    if (contador >= slides.length) {
+        contador = 0;
+    }
+    actualizarSlide();
+});
+
+anteriorBtn.addEventListener("click", () => {
+    contador--;
+    if (contador < 0) {
+        contador = slides.length - 1;
+    }
+    actualizarSlide();
+});
+
+function actualizarSlide() {
+    slides.forEach((slide, index) => {
+        if (index === contador) {
+            slide.style.display = "block";
+        } else {
+            slide.style.display = "none";
+        }
+    });
+}
+
+actualizarSlide();
